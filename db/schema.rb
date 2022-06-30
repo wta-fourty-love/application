0d6c6b9eb09899d9b2d6e2bf9f1074d018b370f3
tmtocb <74333541+tmtocb@users.noreply.github.com>
2022-07-01 01:19:32 +0200

    [App-88/89]: add Country to Database Schema via running the migration (#145)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_224138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "iso_code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end

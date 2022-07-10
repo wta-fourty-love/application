0ace0005a02d8971a60af7f0490b83a5981b1db7
Kuba Nowak <38226932+Thevel0per@users.noreply.github.com>
2022-07-10 23:15:09 +0200

    [App-123]: Create seasons table (#203)
    * [App-123]: Create migration for Seasons table creation
    
    * [App-123]: Update schema with seasons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_205801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "seasons", force: :cascade do |t|
+    t.integer "year", default: -> { "date_part('year'::text, CURRENT_DATE)" }, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

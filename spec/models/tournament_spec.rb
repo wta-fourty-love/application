8ef6d077ef583f2eb38a84ebccce7f92f63f2ddf
Mateusz Tomczak <kittz.esport@gmail.com>
2022-07-15 01:48:02 +0200

    [App-171]: add relationship: Country has many Tournaments, Tournament belongs to Country, add default specs

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Tournament do
     it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false, precision: 6) }
     it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false, precision: 6) }
   end
+
+  describe 'relations' do
+    it { should belong_to(:country) }
+  end
 end

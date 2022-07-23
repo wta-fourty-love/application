93c5814d15ea43397841a2663c4073810ac6acd4
Kuba Nowak <kubanowak9755@gmail.com>
2022-07-23 18:50:59 +0200

    [App-200]: Added mock data to HomeController#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,37 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @matches = [
+      {
+        players: [
+          {
+            name: 'Female Nadal',
+            stats: { some_stat_1: '32%', some_stat_2: '45%' },
+            score: 1
+          },
+          {
+            name: 'Female Hurkacz',
+            stats: { some_stat_1: '62%', some_stat_2: '89%' },
+            score: 2
+          }
+        ]
+      }
+    ]
+
+    @live_matches = [
+      {
+        players: [
+          {
+            name: 'Swiatek',
+            score: 1
+          },
+          {
+            name: 'Sharapova',
+            score: 0
+          }
+        ]
+      }
+    ]
+  end
 end

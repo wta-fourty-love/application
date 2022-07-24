1ffdbc950f0294a55b098d85442580395d005958
tmtocb <74333541+tmtocb@users.noreply.github.com>
2022-07-24 11:26:02 +0200

    [App-219]: add Season 'add' Command, 'add' Contract & specs (#347)

---
+++ app/api/seasons/commands/contracts/add_contract.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Seasons
+  module Commands
+    module Contracts
+      class AddContract < ::Contracts::ApplicationContract
+        params do
+          required(:year).filled(:integer)
+        end
+
+        rule(:year) do
+          key.failure('must_be_unique') if all_seasons_years.include?(values[:year])
+        end
+
+        rule(:year) do
+          key.failure('cannot_be_in_future') if values[:year] > Time.current.year
+        end
+
+        private
+
+        def all_seasons_years
+          @all_seasons_years ||= Season.all.pluck(:year)
+        end
+      end
+    end
+  end
+end

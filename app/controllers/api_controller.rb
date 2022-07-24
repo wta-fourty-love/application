9e84b0dba33ec3a9acfa65fdd8ce574c382269be
Mateusz Tomczak <kittz.esport@gmail.com>
2022-07-24 12:07:51 +0200

    [App-225]: define object non exists error in API Controller with status 400, rescue from ::Exceptions::ObjectNonExistsError

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class ApiController < ActionController::Base
+  rescue_from ::Exceptions::ObjectNonExistsError, with: :object_non_exists_error
   rescue_from ::Exceptions::ValidationError, with: :validation_error
+
+  def object_non_exists_error
+    render json: { error: 'object_non_exists' }, status: 400
+  end
 
   def validation_error(exception)
     render json: { error: exception.errors }, status: 422

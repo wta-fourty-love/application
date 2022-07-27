b191e2f33662e7b6d77526500cbc72f642661c0d
Mateusz Tomczak <kittz.esport@gmail.com>
2022-07-27 17:32:20 +0200

    [App-240]: add Tournament Edition model database columns specs

---
+++ spec/models/tournament_edition_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe TournamentEdition do
+  describe 'database columns' do
+    it { should have_db_column(:starts_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_column(:ends_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false, precision: 6) }
+    it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false, precision: 6) }
+  end
+end

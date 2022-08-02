4bc73fad48e8e5e47f493a4b8e61979d43209f31
Mateusz Tomczak <kittz.esport@gmail.com>
2022-08-02 04:55:46 +0200

    [App-262]: set-up Sentry enabled environments to Development & Production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,6 @@ require 'sentry-ruby'
 
 Sentry.init do |config|
   config.enabled_environments = %w[development, production]
-  config.dsn = Rails.application.credentials.sentry[:dsn] unless Rails.env ='test'
+  config.dsn = Rails.application.credentials.sentry[:dsn] unless Rails.env == 'test'
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 end

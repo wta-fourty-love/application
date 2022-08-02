f4f0ed76182331574238a4bc9b02802b9781ccff
Mateusz Tomczak <kittz.esport@gmail.com>
2022-08-02 12:30:34 +0200

    [App-269]: add Staging to Sentry enabled environments config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 require 'sentry-ruby'
 
 Sentry.init do |config|
-  config.enabled_environments = %w[development, production]
+  config.enabled_environments = %w[development, staging, production]
   config.dsn = Rails.application.credentials.sentry[:dsn] unless Rails.env == 'test'
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
